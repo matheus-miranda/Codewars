79152c12df5e05a707d75570defa3d6098b818ae
Matheus Miranda <15269393+matheus-miranda@users.noreply.github.com>
2021-11-27 16:17:00 -0300

    Optimize Mumbling.kt

--- Mumbling/Mumbling.kt
+++ Mumbling/Mumbling.kt
@@ -1,17 +1,4 @@
 fun accum(s: String): String {
-
-    val stringList = s.toList()
-    val sb = StringBuilder()
-
-    stringList.forEachIndexed { index, c ->
-        val firstChar = c.uppercaseChar()
-        sb.append(firstChar)
-
-        for (i in 1..index) {
-            sb.append(c.lowercaseChar())
-        }
-        sb.append("-")
-    }
-
-    return sb.deleteCharAt(sb.length - 1).toString()
+    return s.mapIndexed { index, char -> char.uppercase() + char.lowercase().repeat(index) }
+        .joinToString("-")
 }
