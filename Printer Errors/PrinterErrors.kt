884e17131eb4e18ad9088cd5ddd09bd25aae97ac
Matheus Miranda <15269393+matheus-miranda@users.noreply.github.com>
2021-12-05 23:29:38 -0300

    Update PrinterErrors.kt

--- Printer Errors/PrinterErrors.kt
+++ Printer Errors/PrinterErrors.kt
@@ -1,12 +1,4 @@
 fun printerError(s: String): String {
-    val range = 'n'..'z'
-    var malfunctionCounter = 0
-
-    s.forEach { letter ->
-        if (letter in range) {
-            malfunctionCounter++
-        }
-    }
-
-    return "$malfunctionCounter/${s.length}"
+    val malfunctionCount = s.filter { it in 'n'..'z' }.count()
+    return "$malfunctionCount/${s.length}"
 }
