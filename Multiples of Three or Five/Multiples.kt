9e754df092f22c3f0e9c63c1c69a25715b0f8dda
Matheus Miranda <15269393+matheus-miranda@users.noreply.github.com>
2021-12-09 14:06:35 -0300

    Update Multiples.kt

--- Multiples of Three or Five/Multiples.kt
+++ Multiples of Three or Five/Multiples.kt
@@ -1,13 +1,1 @@
-fun solution(number: Int): Int {
-    val multiples = mutableListOf(0)
-
-    if (number > 0) {
-        for (i in 1 until number) {
-            if (i % 3 == 0 || i % 5 == 0) {
-                multiples.add(i)
-            }
-        }
-    }
-
-    return multiples.sum()
-}
+fun solution(number: Int): Int = (1 until number).filter { it % 3 == 0 || it % 5 == 0 }.sum()
